919af4c15545c3038561e1a34c1f9f41a5290c05
Jake Boyce <i_am_aphelion@proton.me>
2023-05-07 11:58:53 -0400

    Commit all exercises for "Basics" chapter

---
+++ basics/exercise_1.rb
@@ -0,0 +1,4 @@
+first_name = "Jake"
+last_name = "Boyce"
+
+puts "#{first_name} #{last_name}"
---
+++ basics/exercise_3.rb
@@ -0,0 +1,11 @@
+movies = { fight_club: 1999,
+           scream: 1996,
+           jurassic_park: 1993,
+           interstellar: 2014,
+           the_batman: 2022 }
+
+puts movies[:fight_club]
+puts movies[:scream]
+puts movies[:jurassic_park]
+puts movies[:interstellar]
+puts movies[:the_batman]
